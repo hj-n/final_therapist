4c89088451b9cb82aac00ba1be2ccf42056cd67e
Hyeon Jeon <hj@hcil.snu.ac.kr>
2025-03-29 12:37:51 +0900

    added final therapist

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-
 import styles from "./Header.module.scss";
 
 const Header = () => {
 	return (
 		<div className={styles.header}>
 			<h1>Data Therapist</h1>
+			<div className={styles.exportButtons}>
+				<button type="button">export as csv</button>
+				<button type="button">export as json</button>
+			</div>
 		</div>
-	)
-}
+	);
+};
 
 export default Header;
